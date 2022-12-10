bfe6f21bdc9961444fb2e76639a1a48b7fbcedf9
Isaacoso <iscprepa@gmail.com>
2022-12-10 11:44:35 -0600

    todo

---
+++ components/dashboard/Drawer.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-navigation-drawer
+    absolute
+    permanent
+    left
+    app
+    color="indigo darken-4"
+  >
+    <template #prepend>
+      <v-list-item two-line>
+        <v-list-item-avatar>
+          <img src="https://randomuser.me/api/portraits/men/40.jpg">
+        </v-list-item-avatar>
+
+        <v-list-item-content>
+          <v-list-item-title>Isaac Salas</v-list-item-title>
+          <v-list-item-subtitle>Loggeau</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+    </template>
+
+    <v-divider />
+
+    <v-list dense>
+      <v-list-item
+        v-for="item in items"
+        :key="item.title"
+        :to="item.path" 
+      >
+        <v-list-item-icon>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-item-icon>
+
+        <v-list-item-content>
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      items: [
+        { 
+        title: 'Users', 
+        icon: 'mdi-account-group-outline',
+        path:'/dashboard/users',
+
+
+        },
+        { title: 'My Account', icon: 'mdi-account', path:'/dashboard',},
+      ]
+    }
+  }
+}
+</script>
---
+++ pages/dashboard/users/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-row>
+      <v-col cols="6">
+        <RegisterVue />
+      </v-col>
+      <v-col cols="6">
+        <UsuariosVue />
+      </v-col>
+    </v-row>
+</template>
+
+  <script>
+import RegisterVue from '~/components/users/RegisterVue.vue'
+import UsuariosVue from '~/components/users/UsuariosVue.vue';
+  export default {
+    name: "Index",
+    auth: true, //true
+    components: { RegisterVue, UsuariosVue },
+    layout: "DashboardVue"
+}
+  </script>
